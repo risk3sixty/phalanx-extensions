4f1bd7a3de296340f7a7ee8bbbb4938b13c19386
Cal Supik <calsupik@gmail.com>
2021-05-24 13:08:07 -0400

    add try/catch and remove region

--- aws/ec2/listImages/README.md
+++ aws/ec2/listImages/README.md
@@ -6,4 +6,3 @@ List information about all EC2 images for a particular AWS account.
 
 1. `AWS_ACCESS_KEY_ID` (required): AWS IAM access key
 2. `AWS_SECRET_ACCESS_KEY` (required): AWS IAM access secret
-3. `AWS_REGION` (required): AWS region
--- aws/ec2/listImages/src/listImages.ts
+++ aws/ec2/listImages/src/listImages.ts
@@ -5,15 +5,17 @@ import AWS from 'aws-sdk'
 ;(async function listImages() {
   assert(process.env.AWS_ACCESS_KEY_ID, 'AWS key should be available')
   assert(process.env.AWS_SECRET_ACCESS_KEY, 'AWS secret should be available')
-  assert(process.env.AWS_REGION, 'AWS region should be provided')
 
-  const ec2 = new AWS.EC2({ region: process.env.AWS_REGION })
+  const ec2 = new AWS.EC2()
 
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeImages-property
-  const { Images } = await ec2.describeImages().promise()
-  
-  // console.log(columnify(Images))
-  console.log(JSON.stringify(Images, null, 2))
+  try {
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeImages-property
+    const { Images } = await ec2.describeImages().promise()
+    // console.log(columnify(Images))
+    console.log(JSON.stringify(Images, null, 2))
+  } catch (e) {
+    console.log(e.message)
+  }
 
   process.exit()
 })()
